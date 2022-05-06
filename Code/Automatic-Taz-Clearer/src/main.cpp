9d4c2fbd177dba7ae2e7304543e3f3e567df65a2
Maeve Baksa <maeve@maevebaksa.com>
2022-05-06 03:28:49 -0400

    Update main.cpp

--- Code/Automatic-Taz-Clearer/src/main.cpp
+++ Code/Automatic-Taz-Clearer/src/main.cpp
@@ -1,8 +1,8 @@
 //Maeve Baksa - May 5, 2022 - Automatic TAZ Clearer - This Was Not Written the Day Before the Deadline
 
 #include <Arduino.h>
-#include <AccelStepper.h>
 #include <Servo.h>
+#include <string.h>
 
 
 // ** SETUP AND CALIBRATION WHOHOO ** 
@@ -48,8 +48,7 @@
 // * movement setup *
 
 // enable this (set to 1) in order to calibrate the motor.
-#define stepsPerMMCalibrationLeft 0
-#define stepsPerMMCalibrationRight 0
+#define stepsPerMMCalibration 0
 
 // Utilize the following instructions for calibrating how far the motor has to turn to move 1mm.
 
@@ -68,15 +67,14 @@
 // The above will have to be repeated 2 - 3 times until the distance travaled is correct.
 // Remember to write in the values here between goes -- save them -- then upload them.
 
-// In a pinch, the default values of 2560 for each motor can be used in order to test the system.
+// In a pinch, the default value of 2560 can be used in order to test the system.
 //For production it is HIGHLY reccomended that you calibrate the E-Steps for maximum accuracy.
 
-#define stepsPerMMLeft 2560
-#define stepsPerMMRight 2560
+int stepsPerMM = 400;
 
 //the total distance you want the aparatus to move (in mm)
 
-#define travelDistance 305
+int travelDistance = 305;
 
 
 
@@ -99,32 +97,27 @@
 // Since the two motors will be moving in opposing directions, one of these values
 // should be closer to 0, whereas the other closer to 180. Attempt to make these as straight as possible.
 
-#define rightServoLimit 0
-#define leftServoLimit 180
+int rightServoLimit = 0;
+int leftServoLimit = 180;
 
+// * physical limits*
 
+//the delay between pulses -- if your motor sounds like it is grinding and your VREF has been calibrated
+//try increasing this value to slow the top speed of the motor
 
-
-// * mechanical limits *
-// these not need be changed -- before touching these as 
-// troubleshooting, ensure that your stepper drivers are set to the correct
-// vref. When in doubt -- go slow.
-
-//the left stepper motor's max speed (in steps/sec)
-#define leftStepperMaxSpeed 2000
-//the left stepper motor's acceleration (in steps/sec^2)
-#define leftStepperAccel 200
-//the right stepper motor's max speed (in steps/sec)
-#define rightStepperMaxSpeed 2000
-//the right stepper motor's acceleration (in steps/sec^2)
-#define rightStepperAccel 200
+int delayTime = 500;
 
 //* misc *
 //ignore this
 
 //this is the analogread trigger sensetivity at which point it should read a
 //go command
-#define triggerSensitivity 512
+int triggerSensitivity = 512;
+
+// * define servos *
+//make an instance of the servo class, attach the corresponding pin to it in setup
+Servo rightServo;
+Servo leftServo;
 
 //define the loop checking variable, will make
 //sure that the pin has been high for at least
@@ -132,69 +125,167 @@
 
 int wasHigh = 0;
 
-
 //* debugging *
 //ignore this
 
 //set this to one if you are using the arduino outside of an environment 
 //in which it is connected to an octopi.
-#define overrideTriggerPin 0
+#define overrideTriggerPin 1
 
-// * validate configuration *
 
-//at least, somewhat
-#if (stepsPerMMCalibrationLeft == 1 && stepsPerMMCalibrationRight == 1)
-    #error Please only calibrate one motor at a time, it will safe your time -- trust me on this.
-#endif
+//this is the function in charge of moving the stepper motors.
+//the main points are as follows
+//motor has three options as an int, 1 for left, 2 for right, 3 for both
+//direction has two options as an int, 1 for forward, 2 for backward
+//mm is the distance traveled, which is multiplied with the steps per mm to get total steps
+void moveStepper(int motor, int direction, int mm){
 
-// * define steppers *
+  //calculate steps to take:
+  long int stepsNeeded = abs(mm * stepsPerMM);
+  Serial.println(stepsNeeded);
+  //source: 
+  //https://www.makerguides.com/a4988-stepper-motor-driver-arduino-tutorial/
+  //left motor
+  if (motor == 1){
+    //checks the direction that is desired, and set Dir accordingly
+    if (direction == 1){
+      digitalWrite(leftMotorDirPin, LOW);
+    }
+    else if (direction == 2){
+      digitalWrite(leftMotorDirPin, HIGH);
+    }
+    //repeat this for the amount of pulses needed (mm*steps / mm = steps)
+    for (int i = 0; i < stepsNeeded; i++) {
+    // Pulsing the pin like with PWM will change the speed.
+      digitalWrite(leftMotorStepPin, HIGH);
+      delayMicroseconds(delayTime);
+      digitalWrite(leftMotorDirPin, LOW);
+      delayMicroseconds(delayTime);
+  }
+  }
 
-//first the stepper motors must be initialized, as per the documetnation for accelstepper and this source
-// https://lastminuteengineers.com/a4988-stepper-motor-driver-arduino-tutorial/
-// ^^ the name of this source could not be more accurate ;>
 
-//define the two stepper motors -- classes :>
-//the "DRIVER" option allows for a CNC shield with existing motor drivers instead
-//of utilizing the arduino as the driver itself (better solution this way).
-//The pins are defined above
+  //right motor
+  else if (motor == 2){
+    //checks the direction that is desired, and set Dir accordingly
+    if (direction == 1){
+      digitalWrite(rightMotorDirPin, LOW);
+    }
+    else if (direction == 2){
+      digitalWrite(rightMotorDirPin, HIGH);
+    }
+    for (int i = 0; i < stepsNeeded; i++) {
+    // Pulsing the pin like with PWM will change the speed.
+      digitalWrite(rightMotorStepPin, HIGH);
+      delayMicroseconds(delayTime);
+      digitalWrite(rightMotorStepPin, LOW);
+      delayMicroseconds(delayTime);
+  }
+  }
 
-AccelStepper leftStepper(AccelStepper::DRIVER,leftMotorStepPin,leftMotorDirPin);
-AccelStepper rightStepper(AccelStepper::DRIVER,rightMotorStepPin,rightMotorDirPin);
 
-// * define servos *
-//make an instance of the servo class, attach the corresponding pin to it in setup
-Servo rightServo;
-Servo leftServo;
+  //both motors -- in this case my previous code
+  //was able to treat two steps per mm values 
+  //individually -- this version doesn't do that
+  // but I don't want to rewrite everything
+  // so it is assuming they are close
+  //enough as that is better than them
+  //drifting from different values
+  else if (motor == 3){
+    //checks the direction that is desired, and set Dir accordingly
+    if (direction == 1){
+      digitalWrite(rightMotorDirPin, LOW);
+      digitalWrite(leftMotorDirPin, LOW);
+    }
+    else if (direction == 2){
+      digitalWrite(rightMotorDirPin, HIGH);
+      digitalWrite(leftMotorDirPin, HIGH);
+    }
+
+    for (int i = 0; i < stepsNeeded; i++) {
+     // Pulsing the pin like with PWM will change the speed.
+      digitalWrite(rightMotorStepPin, HIGH);
+      digitalWrite(leftMotorStepPin,HIGH);
+      delayMicroseconds(delayTime);
+      digitalWrite(rightMotorStepPin, LOW);
+      digitalWrite(leftMotorStepPin,LOW);
+      delayMicroseconds(delayTime);
+  }
+  }
+}
+
+//this is the actual clearing script. It is pulled to it's own function as this way it is easier for customization by the end user.
+void clear(){
+  //run the servos to the locations by utilizing the moveStepper function.
+
+  Serial.println("Moving");
+  int i = 0;
+  while (i < travelDistance){
+    moveStepper(3,1,1);
+    i++;
+  }
+  Serial.println("Moved");
+
+  //then bring in the servos to scoop
+  Serial.println("Scooping");
+  rightServo.write(rightServoLimit);
+  leftServo.write(leftServoLimit);
+  Serial.println("Scooped");
+
+  //return the arms to origin repeating the above with 2 for the direction instead of
+  //one to invert it
+
+
+  Serial.println("Clearing");
+  i=0;
+  while (i < travelDistance){
+    moveStepper(3,2,1);
+    i++;
+  }
+  Serial.println("Cleared");
+
+
+  //AT THIS POINT THE PARTS ARE OFF AND LIFE IS GOOD!!!!
+
+  //return the servos back to the rest point
+  Serial.println("Finishing");
+  rightServo.write(90);
+  leftServo.write(90);
+  Serial.println("Finished");
+
+}
+
 
 void setup() {
 
-  //set your limits as defined above -- this is insipired from this code:
-  // https://codebender.cc/sketch:356209#2%20stepper%20motors%20test%20using%20accelstepper%20library.ino
+  Serial.begin(9600);
 
-  rightStepper.setMaxSpeed(rightStepperMaxSpeed);
-  rightStepper.setAcceleration(rightStepperAccel);
-
-  leftStepper.setMaxSpeed(leftStepperMaxSpeed);
-  leftStepper.setAcceleration(leftStepperAccel);
-
+  //pin modes (this took me far too long to realise this bug)
+  pinMode(rightMotorDirPin,OUTPUT);
+  pinMode(rightMotorStepPin,OUTPUT);
+  pinMode(leftMotorStepPin,OUTPUT);
+  pinMode(leftMotorDirPin,OUTPUT);
+  pinMode(leftServoPin,OUTPUT);
+  pinMode(rightServoPin,OUTPUT);
 
   //check if you want to *calibrate*
 
   //the calibration sequence will do as explained above, move the motor 100mm, and using the actual distance you
   // can calculate how far you need to adjust.
   // The movement code, is cited from the source above.
-  //calibrate left
-  if (stepsPerMMCalibrationLeft == 1 && stepsPerMMCalibrationRight == 0) {
-    leftStepper.runToNewPosition(100*stepsPerMMLeft);
+  //calibrate
+  if (stepsPerMMCalibration == 1) {
+    int i = 0;
+    while (i < 100){
+      moveStepper(3,1,1);
+      i++;
+    }
     delay(20000);
-    leftStepper.runToNewPosition(0);
-  }
-  //calibrate right
-  else if (stepsPerMMCalibrationRight == 1 && stepsPerMMCalibrationLeft == 0){
-    rightStepper.runToNewPosition(100*stepsPerMMRight);
-    delay(20000);
-    rightStepper.runToNewPosition(0);
-
+    i = 0;
+    while (i < 100){
+      moveStepper(3,2,1);
+      i++;
+    }
   }
 
   //attach pins to the servos
@@ -206,33 +297,8 @@ void setup() {
   rightServo.write(90);
   leftServo.write(90);
 
-}
 
-void clear(){
-  //this is the actual clearing script. It is pulled to it's own function as this way it is easier for customization by the end user.
 
-  //the code for moving them together calls from this forum post, explaining the two types of motion:
-  //https://forum.arduino.cc/t/running-2-stepper-with-accelstepper-together-and-seperate/387822/2
-
-  //first move the arms to the end point
-
-  leftStepper.moveTo(travelDistance*stepsPerMMLeft);
-  rightStepper.moveTo(travelDistance*stepsPerMMRight);
-  while (!leftStepper.run() || !rightStepper.run());
-
-  //then bring in the servos to scoop
-  rightServo.write(rightServoLimit);
-  leftServo.write(leftServoLimit);
-
-  //return the arms to origin
-  leftStepper.moveTo(0);
-  rightStepper.moveTo(0);
-
-  //AT THIS POINT THE PARTS ARE OFF AND LIFE IS GOOD!!!!
-
-  //return the servos back to the rest point
-  rightServo.write(90);
-  leftServo.write(90);
 
 }
 
